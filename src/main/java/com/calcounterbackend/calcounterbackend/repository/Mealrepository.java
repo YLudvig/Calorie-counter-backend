8f178778a47870eef1c889129f3c15afc06bd7d9
YLudvig <yslu24ne@student.ju.se>
2025-06-26 13:34:09 +0200

    backendlösning för dropdown av tidigare mat

--- src/main/java/com/calcounterbackend/calcounterbackend/repository/Mealrepository.java
+++ src/main/java/com/calcounterbackend/calcounterbackend/repository/Mealrepository.java
@@ -6,17 +6,18 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.calcounterbackend.calcounterbackend.model.Mealitem;
 
 @Repository
-public interface Mealrepository extends JpaRepository <Mealitem, UUID>{
+public interface Mealrepository extends JpaRepository<Mealitem, UUID> {
 
     List<Mealitem> findAllByUserIdAndDate(UUID userId, LocalDate date);
 
     void deleteByMealIdAndUserId(UUID mealId, UUID userId);
 
-
-    
+    @Query(value = "SELECT * FROM mealitem ORDER BY date DESC, name ASC", nativeQuery = true)
+    List<Mealitem> findAllByUserId(UUID userId);
 }
