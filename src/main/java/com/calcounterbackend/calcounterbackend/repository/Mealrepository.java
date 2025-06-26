d32d2bcce90e066ba95fda9e65d242120e24f468
YLudvig <yslu24ne@student.ju.se>
2025-06-26 13:58:15 +0200

    nyttjar sql för bättre hantering och mindre frontend datamanipulering

--- src/main/java/com/calcounterbackend/calcounterbackend/repository/Mealrepository.java
+++ src/main/java/com/calcounterbackend/calcounterbackend/repository/Mealrepository.java
@@ -18,6 +18,20 @@ public interface Mealrepository extends JpaRepository<Mealitem, UUID> {
 
     void deleteByMealIdAndUserId(UUID mealId, UUID userId);
 
-    @Query(value = "SELECT * FROM mealitem ORDER BY date DESC, name ASC", nativeQuery = true)
+    //SQL query som hanterar filtrering och sortering på backend och skickar en snygg lista till frontend
+    @Query(value = """
+            SELECT m.*
+            FROM mealitem m
+            JOIN (
+                SELECT name, calories, protein, MAX(date) as max_date
+                FROM mealitem
+                WHERE user_id = :userId
+                GROUP BY name, calories, protein
+            ) grouped ON m.name = grouped.name
+                    AND m.calories = grouped.calories
+                    AND m.protein = grouped.protein
+                    AND m.date = grouped.max_date
+            ORDER BY m.date DESC, m.name ASC
+            """, nativeQuery = true)
     List<Mealitem> findAllByUserId(UUID userId);
 }
